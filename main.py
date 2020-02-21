bc19ca756e4dc491d64d1c7ca5b9d0f1b43b3df2
kjthorpe18 <kjt29@pitt.edu>
2020-02-21 13:36:37 -0500

    Made static dir, minor changes

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-from flask import Flask
-from flask import Response
-
+from flask import Flask, Response, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def root():
-    return Response('This is NOT HTML.', mimetype='text/plain')
+    return redirect("/static/index.html", code=302)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
