fb526859e1b28152bc5ae041202bd876c4e017ee
Jared Frank <jfrank1120@verizon.net>
2020-03-29 14:32:14 -0400

    Updates
    -Changed some of the html to the correct versions
    - Appended scripts.js file to add in Alex's  functions
    - Still have to check functionality before merger

--- static/scripts.js
+++ static/scripts.js
@@ -1,3 +1,93 @@
+//--------SERVER_ACCES.JS STARTS HERE ----
+
+
+function createXmlHttp() {
+    var xmlhttp;
+    if (window.XMLHttpRequest) {
+        xmlhttp = new XMLHttpRequest();
+    } else {
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    if (!(xmlhttp)) {
+        alert("Your browser does not support AJAX!");
+    }
+    return xmlhttp;
+}
+
+// this function converts a simple key-value object to a parameter string.
+function objectToParameters(obj) {
+    var text = '';
+    for (var i in obj) {
+        // encodeURIComponent is a built-in function that escapes to URL-safe values
+        text += encodeURIComponent(i) + '=' + encodeURIComponent(obj[i]) + '&';
+    }
+    return text;
+}
+
+
+function postParameters(xmlHttp, target, parameters) {
+    if (xmlHttp) {
+        xmlHttp.open("POST", target, true); // XMLHttpRequest.open(method, url, async)
+        var contentType = "application/x-www-form-urlencoded";
+        xmlHttp.setRequestHeader("Content-type", contentType);
+        xmlHttp.send(parameters);
+    }
+}
+
+function sendJsonRequest(parameterObject, targetUrl, callbackFunction) {
+    var xmlHttp = createXmlHttp();
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4) {
+            console.log(xmlHttp.responseText);
+            var myObject = JSON.parse(xmlHttp.responseText);
+            callbackFunction(myObject, targetUrl, parameterObject);
+        }
+    }
+    console.log(targetUrl);
+    console.log(parameterObject);
+    postParameters(xmlHttp, targetUrl, objectToParameters(parameterObject));
+}
+
+
+//-------END OF SERVER_ACCESS.JS
+
+
+
+
+
+// This can load data from the server using a simple GET request.
+function getData(targetUrl, callbackFunction) {
+    let xmlHttp = createXmlHttp();
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4) {
+            // note that you can check xmlHttp.status here for the HTTP response code
+            try {
+                let myObject = JSON.parse(xmlHttp.responseText);
+                callbackFunction(myObject, targetUrl);
+            } catch (exc) {
+                console.log("There was a problem at the server.");
+            }
+        }
+    }
+    xmlHttp.open("GET", targetUrl, true);
+    xmlHttp.send();
+}
+
+
+function showError(msg) {
+    let errorAreaDiv = document.getElementById('ErrorArea');
+    errorAreaDiv.display = 'block';
+    errorAreaDiv.innerHTML = msg;
+}
+
+
+function hideError() {
+    let errorAreaDiv = document.getElementById('ErrorArea');
+    errorAreaDiv.display = 'none';
+}
+
+
+
 //edited into garage CALLBACKFUNCTION
 function garageSaved(result, targetUrl, params) {
     if (result && result.ok) {
@@ -7,6 +97,8 @@ function garageSaved(result, targetUrl, params) {
         showError(result.error);
     }
 }
+
+
 
 
 //Saves a new garage after user inputs one
